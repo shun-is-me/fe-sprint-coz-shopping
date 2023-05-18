7543ab4b0e9b9d201bab0cd2ac06391c5176fae3
shun-is-me <rhkrtlgus@naver.com>
2023-05-18 12:50:30 +0900

    header 완료

---
+++ src/pages/mainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function mainPage() {
+  return (
+    <div>
+      <h1>Welcome to the Main Page</h1>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default mainPage;
